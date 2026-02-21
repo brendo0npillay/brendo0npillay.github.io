9888f064c49df85517e3f1028772dd0d1eba7725
brendon9909 <brendonpillay02@gmail.com>
2026-02-21 13:21:29 +0200

    changed portfolio look

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-//html elements
-const menu = document.querySelector(".menuIcon")
-const menuItems = document.querySelector(".navContainer")
-const exit = document.querySelector(".exitIcon")
+const menuBtn = document.querySelector(".menuImg")
+const burgerMenu = document.querySelector(".burgerItems")
+const menuImg = document.querySelector(".menuImg")
 
-//variables
-let clicked = false
+const form = document.getElementById("contactForm")
+const publicKey = "huqCR8EwTkgK94aWK"
+const templateID = "template_gpxuvo5"
+const serviceID = "service_ixm37zr"
 
-//menu icon click event
-menu.onclick = () => {
-    menuItems.style.display = "inline"
-    exit.style.display = "inline"
-    menu.style.display = "none"
+const menuIcon = "./assets/icons8-menu-50 (1).png"
+const xIcon = "./assets/icons8-close-50.png"
 
+menuBtn.onclick = () => {
+    burgerMenu.classList.toggle("show")
+    
+    if(burgerMenu.classList.contains("show")){
+        menuImg.src = xIcon
+    } else{
+        menuImg.src = menuIcon
+    }
 }
 
-//exit icon click event
-exit.onclick = () => {
-    menuItems.style.display = "none"
-    exit.style.display = "none"
-    menu.style.display = "inline"
-}
+emailjs.init(publicKey)
+
+form.addEventListener("submit", function(e){
+    e.preventDefault()
+
+    emailjs.sendForm(
+        serviceID,
+        templateID,
+        this
+    ).then(() => {
+        alert("message sent")
+        form.reset()
+    }).catch((err) => {
+        alert("message failed to send")
+        console.error(err)
+    })
+})
